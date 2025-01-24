1a106ef04d4f6181ca4982894994ab97d79064cd
Bigandres_dev <jandres20003@gmail.com>
2025-01-24 16:37:58 -0500

    Create Bigandres.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Bigandres.py
@@ -0,0 +1,45 @@
+"""
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ 
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ 
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ 
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ 
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+"""
+
+
+# Este es el URL de la pagina de python  https://www.python.org/
+
+#Este es una comentario en una linea
+
+"""
+Este es un comentario en 
+varias lineas en visual studio code
+
+"""
+
+my_variable = "Hola mundo"
+
+MY_CONSTANT = "No me cambies porfavor"
+
+
+#Variables primitivas 
+
+my_int: int = 1 
+my_float: float = 1.5
+my_bool: bool = True 
+my_bool = False 
+my_string: str = "Bienvenido Jorge"
+my_other_string = "bienvenido Andres"
+
+print("hola", "python")
